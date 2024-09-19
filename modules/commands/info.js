46463290d3d9b4fb34749734ff17f019ad0f8960
Sujon Be'vies <159610261+sujonbbz@users.noreply.github.com>
2024-09-19 13:38:57 +0600

    Update info.js

--- modules/commands/info.js
+++ modules/commands/info.js
@@ -31,20 +31,17 @@ var callback = () => api.sendMessage({body:`আসসালামু আলা�
 Credit: Ariyan Ahmed
 
 
-𝐍𝐚𝐦𝐞       : ᴀʀᴜᴜㅤ
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : AriYan AhMed
+𝐍𝐚𝐦𝐞       : HORNY MINHAZ
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : MinHaz SixNinE
 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
-𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: Khulna
-𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: Khulna, Dhaka
+𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: Dhaka
+𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: Gazipur, Dhaka
 𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝐌𝐚𝐥𝐞
 𝐀𝐠𝐞           : 𝟏7+
 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : ☹︎
-𝐖𝐨𝐫𝐤        : PYTHON CODER
-𝐆𝐦𝐚𝐢𝐥       : mrsladen2m@gmail.com
-𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801885263191
-𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/@sujonbevies
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/61561264605347`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
-    fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/61556196058001/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
-fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
-   };
+𝐖𝐨𝐫𝐤        : BOT CREATOR
+𝐆𝐦𝐚𝐢𝐥       :!! 
+𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩:!! 
+𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  :!! 
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/61564055655700'
+};
